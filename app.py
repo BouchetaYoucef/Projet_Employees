06b62993720973de3005ef798a02daa860ce6bea
Firstname Lastname <youcef.boucheta.simplon@gmail.com>
2023-01-09 13:50:33 +0100

    new_app

--- app.py
+++ app.py
@@ -369,5 +369,5 @@ def predict():
         return render_template ('index.html',prediction_text='Employee Might Leave The Job')
 
 
-if __name__ == "__main__":
-    app.run (debug=True)
+# if __name__ == "__main__":
+#     app.run (debug=True)
