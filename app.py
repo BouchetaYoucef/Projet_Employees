edfd22b750ea9d3eef9b8c42a99b67514e344899
Firstname Lastname <youcef.boucheta.simplon@gmail.com>
2023-01-09 14:25:08 +0100

    new_app

--- app.py
+++ app.py
@@ -135,11 +135,11 @@ def main():
         
         return inp
     
-    df = input_features()
-    model = pickle.load(open('model1.pkl','rb'))
-    ans = model.predict_proba([df])[0][0]
-    ans = round(100*ans,2)
-    st.subheader(f'The probability of employee being released is {ans} percent.')
+    # df = input_features()
+    # model = pickle.load(open('model1.pkl','rb'))
+    # ans = model.predict_proba([df])[0][0]
+    # ans = round(100*ans,2)
+    # st.subheader(f'The probability of employee being released is {ans} percent.')
     
 if __name__ == "__main__" :
     main()
