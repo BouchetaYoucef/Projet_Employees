0ee5ee4bffd455fdd8341d0eab93a7197dc0e600
Firstname Lastname <youcef.boucheta.simplon@gmail.com>
2023-01-15 15:13:10 +0100

    new_app

--- app.py
+++ app.py
@@ -50,20 +50,6 @@ def main():
         if involve == "":
             job_inv = 1
         elif involve == "":
-       
-         role = st.selectbox('Job Role', ['Research Director','Manager','Healthcare Representative','Manufacturing Director','Lab Technician','Research Scientist','Sales Executive','HR','Sales Representative'])
-        if role == "Research Director" :
-            jobrole = 0
-        elif role == "Manager" or "Healthcare Representative" or "Manufacturing Director":
-            jobrole = 1
-        elif role == "Lab Technician" :
-            jobrole = 3
-        elif role == "Research Scientist" or "Sales Executive" :
-            jobrole = 2
-        elif role == "HR" :
-            jobrole = 4
-        else :
-            jobrole = 5
             
         EverBenched = st.selectbox('EverBenched', ['Yes','No'])
         if EverBenched == "Yes" :
@@ -73,51 +59,9 @@ def main():
         else :
             EverBenched = 3
             
-        mar = st.selectbox('Marital Status',['Married','Divorced','Single'])
-        if mar == "Married" or "Divorced" :
-            mar_stat = 0
-        else :
-            mar_stat = 1
-            
         Joining = st.sidebar.slider('JoiningYear', 2012, 2013,2014,2015,2016,2017,2018)
-        
-        num_worked = st.number_input('Number of companies worked',0.,10.,step = 1.)
-        if num_worked <=4 :
-            num_com = 0
-        else :
-            num_com = 1
             
-        otime = st.selectbox("Does the employee work overtime?",['No', 'Yes'])
-        if otime == "No" :
-            overtime = 0
-        else :
-            overtime = 1
-            
-        sol = st.selectbox('Stock of employee in the company', ["No stocks", "Moderate stocks", "Lots of stocks"])
-        if sol == "No stocks" :
-            stocks = 0 
-        elif sol == "Moderate stocks" :
-            stocks = 1 
-        else :
-            stocks = 2
-            
-        ExperienceInCurrentDomain = st.number_input("Experience In Current Domain",0.,7.,step = 1.)
-        
-        training = st.number_input("Number of times employee did training",0.,7., step = 1.)
-        
-        years_com = st.number_input("Number of years spent in company",0.,30., step = 1.)
-        
-        years_role = st.number_input("Number of years in current role",0.,30., step = 1.)
-        
-        ym = st.number_input("Years with current manager",0.,20., step = 1.)
-        if ym == 0 :
-            years_man = 0 
-        elif ym < 12 :
-            years_man = 1
-        else :
-            years_man = 2
-            
-        inp = [age,City,education, Joining, gender,EverBenched,mar_stat,num_com,overtime, stocks,ExperienceInCurrentDomain,training,years_com,years_role,years_man]
+        inp = [age,City,education, Joining, gender,EverBenched]
         
         return inp
     
