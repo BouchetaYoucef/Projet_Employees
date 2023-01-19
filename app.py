7dc1a912ad66c6c2baa2520108532f47cf346f54
Firstname Lastname <youcef.boucheta.simplon@gmail.com>
2023-01-19 10:22:39 +0100

    New app

--- app.py
+++ app.py
@@ -1,13 +1,110 @@
+# # https://gist.github.com/FranzDiebold/898396a6be785d9b5ca6f3706ef9b0bc
+# """Hack to add per-session state to Streamlit.
+# Works for Streamlit >= v0.65
+# Usage
+# -----
+# import SessionState
+
+# session_state = SessionState.get(user_name='', favorite_color='black')
+# session_state.user_name
+# ''
+# session_state.user_name = 'Mary'
+# session_state.favorite_color
+# 'black'
+# Since you set user_name above, next time your script runs this will be the
+# result:
+# session_state = get(user_name='', favorite_color='black')
+# session_state.user_name
+# 'Mary'
+# """
+
+# import streamlit.report_thread as ReportThread
+# from streamlit.server.server import Server
+
+
+# class SessionState():
+#     """SessionState: Add per-session state to Streamlit."""
+#     def __init__(self, **kwargs):
+#         """A new SessionState object.
+#         Parameters
+#         ----------
+#         **kwargs : any
+#             Default values for the session state.
+#         Example
+#         -------
+#          session_state = SessionState(user_name='', favorite_color='black')
+#          session_state.user_name = 'Mary'
+#         ''
+#         session_state.favorite_color
+#         'black'
+#         """
+#         for key, val in kwargs.items():
+#             setattr(self, key, val)
+
+
+# def get(**kwargs):
+#     """Gets a SessionState object for the current session.
+#     Creates a new object if necessary.
+#     Parameters
+#     ----------
+#     **kwargs : any
+#         Default values you want to add to the session state, if we're creating a
+#         new one.
+#     Example
+#     -------
+#      session_state = get(user_name='', favorite_color='black')
+#      session_state.user_name
+#     ''
+#      session_state.user_name = 'Mary'
+#      session_state.favorite_color
+#     'black'
+#     Since you set user_name above, next time your script runs this will be the
+#     result:
+#      session_state = get(user_name='', favorite_color='black')
+#      session_state.user_name
+#     'Mary'
+#     """
+#     # Hack to get the session object from Streamlit.
+
+#     session_id = ReportThread.get_report_ctx().session_id
+#     session_info = Server.get_current()._get_session_info(session_id)
+
+#     if session_info is None:
+#         raise RuntimeError('Could not get Streamlit session object.')
+
+#     this_session = session_info.session
+
+#     # Got the session object! Now let's attach some state into it.
+
+#     if not hasattr(this_session, '_custom_session_state'):
+#         this_session._custom_session_state = SessionState(**kwargs)
+
+#     return this_session._custom_session_state
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import json
 
 import requests
 import streamlit as st
 
-def run():
-    img1 = open('Employees.jpg')
-    img1 = img1.resize((150,150))
-    st.image(img1,use_column_width=False)
-run()
+# def run():
+#     img1 = open('Employees.jpg')
+#     img1 = img1.resize((150,150))
+#     st.image(img1,use_column_width=False)
+# run()
 
 def get_inputs():
     """Get inputs from users on streamlit"""
@@ -42,7 +139,7 @@ def write_predictions(data: dict):
         data_json = json.dumps(data)
 
         prediction = requests.post(
-            # "https://employee-predict-1.herokuapp.com/predict",
+            "https://employee-predict-1.herokuapp.com/predict",
             headers={"content-type": "application/json"},
             data=data_json,
         ).text[0]
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import hydra
-from evaluate_model import evaluate
-from process import process_data
-from train_model import train
+# import hydra
+# from evaluate_model import evaluate
+# from process import process_data
+# from train_model import train
 
-@hydra.main(version_base=None, config_path="../../config", config_name="main")
-def main(config):
-    process_data(config)
-    train(config)
-    evaluate(config)
+# @hydra.main(version_base=None, config_path="../../config", config_name="main")
+# def main(config):
+#     process_data(config)
+#     train(config)
+#     evaluate(config)
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
